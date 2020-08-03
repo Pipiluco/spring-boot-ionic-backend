ad48b3dd18eca05ac2a6b8f9773f676e31996768
Lucas Francisco <lucasf.silva@live.com>
2020-08-03 19:27:23 -0300

    Ajustes no endpoint /categoria/{id}

--- src/main/java/life/pifrans/cursomc/services/CategoriaService.java
+++ src/main/java/life/pifrans/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import life.pifrans.cursomc.domain.Categoria;
 import life.pifrans.cursomc.repositories.CategoriaRepository;
+import life.pifrans.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,6 +16,6 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
