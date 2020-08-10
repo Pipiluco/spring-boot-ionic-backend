b1e784c463158000585df4eef53da69d35fd8d5f
Lucas Francisco <lucasf.silva@live.com>
2020-08-10 16:27:59 -0300

    Endpoint /cliente/{id} disponível

---
+++ src/main/java/life/pifrans/cursomc/services/ClienteService.java
@@ -0,0 +1,21 @@
+package life.pifrans.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import life.pifrans.cursomc.domain.Cliente;
+import life.pifrans.cursomc.repositories.ClienteRepository;
+import life.pifrans.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	@Autowired
+	private ClienteRepository repository;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
