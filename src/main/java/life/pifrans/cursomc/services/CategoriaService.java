06d65f876704fc5afcee54f1318b2cdb4eb32676
Aluno <Aluno@SN-0344739>
2020-08-14 10:24:00 -0300

    Deletando uma categoria com DELETE

--- src/main/java/life/pifrans/cursomc/services/CategoriaService.java
+++ src/main/java/life/pifrans/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package life.pifrans.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import life.pifrans.cursomc.domain.Categoria;
 import life.pifrans.cursomc.repositories.CategoriaRepository;
+import life.pifrans.cursomc.services.exceptions.DataIntegrityException;
 import life.pifrans.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,14 +20,23 @@ public class CategoriaService {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new  DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
 	}
 }
