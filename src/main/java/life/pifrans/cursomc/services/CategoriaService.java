70da5595d414645ea8ade47c42f34b77756a0fc3
Aluno <Aluno@SN-0344739>
2020-08-14 13:31:03 -0300

    Validação sintática com Bean Validation

--- src/main/java/life/pifrans/cursomc/services/CategoriaService.java
+++ src/main/java/life/pifrans/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import life.pifrans.cursomc.domain.Categoria;
+import life.pifrans.cursomc.dto.CategoriaDTO;
 import life.pifrans.cursomc.repositories.CategoriaRepository;
 import life.pifrans.cursomc.services.exceptions.DataIntegrityException;
 import life.pifrans.cursomc.services.exceptions.ObjectNotFoundException;
@@ -51,5 +52,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
