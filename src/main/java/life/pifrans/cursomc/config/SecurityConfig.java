f1f70e1889498752866360c2e0f4089ce9da2c35
Lucas Francisco <lucasf.silva@live.com>
2020-09-15 21:15:30 -0300

    Implementando autorização

--- src/main/java/life/pifrans/cursomc/config/SecurityConfig.java
+++ src/main/java/life/pifrans/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import life.pifrans.cursomc.security.JWTAuthenticationFilter;
+import life.pifrans.cursomc.security.JWTAuthorizationFilter;
 import life.pifrans.cursomc.security.JWTUtil;
 
 @Configuration
@@ -41,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
--- src/main/java/life/pifrans/cursomc/security/JWTUtil.java
+++ src/main/java/life/pifrans/cursomc/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -21,4 +22,35 @@ public class JWTUtil {
 	public String generateToken(String username) {
 		return Jwts.builder().setSubject(username).setExpiration(new Date(System.currentTimeMillis() + expiration)).signWith(SignatureAlgorithm.HS512, secret.getBytes()).compact();
 	}
+
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date nowDate = new Date(System.currentTimeMillis());
+			if (username != null && expirationDate != null && nowDate.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		}catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			return claims.getSubject();
+		}
+		return null;
+	}
+
 }
